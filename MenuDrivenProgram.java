00bf84af1219ccdcb3bebe7885ef8494c2f33798
Jesrael <mananquiljesrael_bscrim@plmun.edu.ph>
2025-03-01 22:54:21 +0800

    Submitting OBJPROG Week 05 - Laboratory # 20

---
+++ MenuDrivenProgram.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class MenuProgram {
+
+    public static void displayMenu() {
+        System.out.println("Menu:");
+        System.out.println("1. Greet User");
+        System.out.println("2. Check Even/Odd");
+        System.out.println("3. Exit");
+    }
+
+    public static void handleMenuChoice(int choice) {
+        switch (choice) {
+            case 1:
+                greetUser();
+                break;
+            case 2:
+                checkEvenOrOdd();
+                break;
+            case 3:
+                System.out.println("Exiting program. Goodbye!");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid choice. Please enter a number between 1 and 3.");
+        }
+    }
+
+    public static void greetUser() {
+        System.out.println("Hello! Welcome!");
+    }
+
+    public static void checkEvenOrOdd() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        
+        while (!scanner.hasNextInt()) {
+            System.out.println("Invalid input. Please enter a valid integer.");
+            scanner.next();
+            System.out.print("Enter an integer: ");
+        }
+        
+        int number = scanner.nextInt();
+        if (number % 2 == 0) {
+            System.out.println(number + " is an Even number.");
+        } else {
+            System.out.println(number + " is an Odd number.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            displayMenu();
+            System.out.print("Enter your choice (1-3): ");
+            
+            while (!scanner.hasNextInt()) {
+                System.out.println("Invalid input. Please enter a number between 1 and 3.");
+                scanner.next();
+                System.out.print("Enter your choice (1-3): ");
+            }
+            
+            int choice = scanner.nextInt();
+            handleMenuChoice(choice);
+        }
+    }
+}
